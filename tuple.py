44091c01e9da68404acc9a97ea980bdfac035193
So Eun Choi <dmssh77@naver.com>
2019-05-10 21:49:34 +0900

    20190509

--- tuple.py
+++ tuple.py
@@ -8,5 +8,27 @@ print(t, type(t))
 # 시퀀스 연산
 #
 
+# 인덱싱
+print(t[-3], t[-2], t[-1], t[0], t[1], t[2])
+
+# 슬라이싱
+print(t[1:3])
+print(t[:])
+
+# 반복
+t2 = t * 2
+print(t2)
+
+# 연결
+t3 = t + (4, 5, 6)
+print(t3)
+
+# in, not in
+print(5 in t3)
+
+# tuple 변경 불가 ( immutable ) sequence 형
+
+
+
 #
 # 객체 함수
